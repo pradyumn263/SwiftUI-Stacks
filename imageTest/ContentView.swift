7a061d207249ea0e94a641acce10db535c602bea
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-04 14:14:05 +0530

    Initial Commit

---
+++ imageTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  imageTest
+//
+//  Created by Pradyumn Shukla on 04/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ imageTest/imageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageTestApp.swift
+//  imageTest
+//
+//  Created by Pradyumn Shukla on 04/01/21.
+//
+
+import SwiftUI
+
+@main
+struct imageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
